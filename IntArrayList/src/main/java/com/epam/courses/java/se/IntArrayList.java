0b3948e908af787c5abd05ae59eca69142082be3
NameOfTheLaw <tsibin.andr@gmail.com>
2017-02-03 14:07:25 +0300

    Added IntArrayList

---
+++ IntArrayList/src/main/java/com/epam/courses/java/se/IntArrayList.java
@@ -0,0 +1,149 @@
+package com.epam.courses.java.se;
+
+import java.util.Arrays;
+
+/**
+ * Represents the list structure for int values.
+ *
+ * Based on <code>int[]</code>.
+ * Size of the <code>IntArrayList</code> object is dynamic and depends on the last index of the inserted value.
+ * Once you add an <code>i</code> value in the list, size of it would always be more than for <code>i</code> values.
+ * Created by andrey on 02.02.2017.
+ */
+public class IntArrayList {
+
+    private int[] data;
+    private int size;
+
+    public IntArrayList() {
+        this.data = new int[10];
+        size = 0;
+    }
+
+    public IntArrayList(int[] data) {
+        this.data = Arrays.copyOf(data, data.length);
+        size = data.length;
+    }
+
+    /**
+     * Add a value to the list.
+     *
+     * @param value
+     */
+    public void add(int value) {
+        ensureCapasity(size + 1);
+        data[size] = value;
+        size += 1;
+    }
+
+    /**
+     * Returns to <code>i</code> value in the list.
+     *
+     * @param i
+     * @return value
+     */
+    public int get(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return data[i];
+    }
+
+    /**
+     * Removes an element by the index from the list.
+     *
+     * @param i
+     */
+    public void remove(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int j = i; j < size-1; j++) {
+            data[j] = data[j+1];
+        }
+        size -= 1;
+    }
+
+    /**
+     * Sort the list of the elements with increasing.
+     *
+     * Uses <code>mergeSort()</code>.
+     */
+    public void sort() {
+        mergeSort(data, 0, size(), new int[size()]);
+    }
+
+    private void mergeSort(int[] data, int startInclusive, int endExclusive, int[] free) {
+        int length = endExclusive - startInclusive;
+        if (length <= 1) {
+            return;
+        }
+
+        int mid = startInclusive + length/2;
+
+        mergeSort(data, startInclusive, mid, free);
+        mergeSort(data, mid, endExclusive, free);
+
+        merge(data, startInclusive, mid, endExclusive, free);
+    }
+
+    private void merge(int[] data, int startInclusive, int mid, int endExclusive, int[] free) {
+        System.arraycopy(data, startInclusive, free, startInclusive, endExclusive - startInclusive);
+
+        int i = startInclusive;
+        int j = mid;
+
+        for (int k = startInclusive; k < endExclusive; k++) {
+            if (i >= mid) data[k] = free[j++];
+            else if (j >= endExclusive) data[k] = free[i++];
+            else if (free[i] > free [j]) data[k] = free[j++];
+            else data[k] = free[i++];
+        }
+    }
+
+    /**
+     * Returns the index of the given value or index to insert in the sorted collection.
+     *
+     * @param value
+     * @return index of the value or -indexToInsert-1
+     */
+    public int indexOf(int value) {
+        return binarySearch(data, 0, size, value);
+    }
+
+    /**
+     * Current size of the list
+     *
+     * @return <code>index + 1</code> of the last element in the list or <code>0</code> if list is empty.
+     */
+    public int size() {
+        return size;
+    }
+
+    private int getCapacity() {
+        return data.length;
+    }
+
+    private void ensureCapasity(int requiredCapasity) {
+        if (requiredCapasity <= getCapacity()) {
+            return;
+        }
+        int newCapacity = Math.max(requiredCapasity, (getCapacity() * 3) / 2 + 1);
+        data = Arrays.copyOf(data, newCapacity);
+    }
+
+    private int binarySearch(int[] data, int startInclusive, int endExclusive, int value) {
+        int length = endExclusive - startInclusive;
+        if (length == 0) {
+            return -startInclusive-1;
+        }
+        int mid = startInclusive + length/2;
+        if (data[mid] == value) {
+            return mid;
+        } else if (data[mid] > value) {
+            return binarySearch(data, startInclusive, mid, value);
+        } else {
+            return binarySearch(data, mid + 1, endExclusive, value);
+        }
+    }
+}
---
+++ IntArrayList/src/test/java/com/epam/courses/java/se/IntArrayListTest.java
@@ -0,0 +1,106 @@
+package com.epam.courses.java.se;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andrey on 03.02.2017.
+ */
+public class IntArrayListTest {
+
+    @Test
+    public void add() throws Exception {
+        IntArrayList arrayList = new IntArrayList();
+        assertEquals(-1, arrayList.indexOf(10));
+        arrayList.add(10);
+        assertEquals(0, arrayList.indexOf(10));
+        assertEquals(-1, arrayList.indexOf(-5));
+        arrayList.add(-5);
+        assertEquals(1, arrayList.indexOf(-5));
+        arrayList.add(7);
+        assertEquals(2, arrayList.indexOf(7));
+    }
+
+    @Test
+    public void remove() throws Exception {
+        IntArrayList arrayList = new IntArrayList();
+        assertEquals(-1, arrayList.indexOf(5));
+        arrayList.add(5);
+        assertEquals(0, arrayList.indexOf(5));
+        arrayList.add(7);
+        assertEquals(1, arrayList.indexOf(7));
+        assertEquals(2, arrayList.size());
+        arrayList.remove(0);
+        assertEquals(-1, arrayList.indexOf(5));
+        assertEquals(0, arrayList.indexOf(7));
+        assertEquals(1, arrayList.size());
+    }
+
+    @Test
+    public void get() throws Exception {
+        IntArrayList arrayList = new IntArrayList(new int[] {3, 4, 7, 8});
+        assertEquals(7, arrayList.get(2));
+        arrayList.remove(1);
+        assertEquals(7, arrayList.get(1));
+        assertEquals(8, arrayList.get(2));
+        arrayList.remove(0);
+        arrayList.remove(0);
+        assertEquals(8, arrayList.get(0));
+        arrayList.add(9);
+        assertEquals(9, arrayList.get(1));
+    }
+
+    @Test
+    public void size() throws Exception {
+        IntArrayList arrayList = new IntArrayList(new int[] {4, 5, 7});
+        assertEquals(3, arrayList.size());
+        arrayList.add(5);
+        assertEquals(4, arrayList.size());
+        arrayList.add(8);
+        arrayList.remove(0);
+        assertEquals(4, arrayList.size());
+        arrayList.remove(0);
+        assertEquals(3, arrayList.size());
+    }
+
+    @Test
+    public void sort() throws Exception {
+        final int[] ints = {12, 0, -13, 666, 2, 56, 56, 56, 120, -1, 1, 0, Integer.MAX_VALUE, Integer.MIN_VALUE};
+        final int[] expected = Arrays.copyOf(ints, ints.length);
+        Arrays.sort(expected);
+
+        final IntArrayList list = new IntArrayList(ints);
+
+        list.sort();
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals("i = " + i, expected[i], list.get(i));
+        }
+    }
+
+    @Test
+    public void indexOf() throws Exception {
+        int[] testData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        IntArrayList arrayList = new IntArrayList(testData);
+
+        assertEquals(9, arrayList.indexOf(10));
+        assertEquals(0, arrayList.indexOf(1));
+        assertEquals(1, arrayList.indexOf(2));
+        assertEquals(3, arrayList.indexOf(4));
+
+        assertEquals(-1, arrayList.indexOf(-5));
+        assertEquals(-11, arrayList.indexOf(15));
+
+        testData = new int[] {-5, 1, 3, 7, 10};
+        arrayList = new IntArrayList(testData);
+
+        assertEquals(-4, arrayList.indexOf(4));
+        assertEquals(-3, arrayList.indexOf(2));
+        assertEquals(0, arrayList.indexOf(-5));
+        assertEquals(-1, arrayList.indexOf(-6));
+    }
+
+}
