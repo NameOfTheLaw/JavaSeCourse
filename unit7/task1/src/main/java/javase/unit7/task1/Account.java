13ed4ae0fb198a793e2a82f08702cddb676d1e4f
NameOfTheLaw <tsibin.andr@gmail.com>
2017-03-18 23:27:24 +0300

    Added equals, hashcode and toString methods (unit7 task1)

--- unit7/task1/src/main/java/javase/unit7/task1/Account.java
+++ unit7/task1/src/main/java/javase/unit7/task1/Account.java
@@ -10,4 +10,26 @@ public class Account {
     public int getId() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+
+        return id == account.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                '}';
+    }
 }
--- unit7/task1/src/main/java/javase/unit7/task1/Transfer.java
+++ unit7/task1/src/main/java/javase/unit7/task1/Transfer.java
@@ -10,6 +10,15 @@ public class Transfer {
     public Transfer(Account accountFrom, Account accountTo, int sum) {
         Objects.requireNonNull(accountFrom);
         Objects.requireNonNull(accountTo);
+
+        validateTransfer(accountFrom, accountTo, sum);
+
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+        this.sum = sum;
+    }
+
+    private void validateTransfer(Account accountFrom, Account accountTo, int sum) {
         if (accountFrom.equals(accountTo)) {
             throw new IllegalTransferException(
                     String.format("Accounts %d must be different.", accountFrom.getId()));
@@ -18,10 +27,6 @@ public class Transfer {
             throw new IllegalTransferException(
                     String.format("Sum %d should be bigger than 0.", sum));
         }
-
-        this.accountFrom = accountFrom;
-        this.accountTo = accountTo;
-        this.sum = sum;
     }
 
     public Account getAccountFrom() {
@@ -34,5 +39,35 @@ public class Transfer {
 
     public int getSum() {
         return sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transfer transfer = (Transfer) o;
+
+        if (sum != transfer.sum) return false;
+        if (accountFrom != null ? !accountFrom.equals(transfer.accountFrom) : transfer.accountFrom != null)
+            return false;
+        return accountTo != null ? accountTo.equals(transfer.accountTo) : transfer.accountTo == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = accountFrom != null ? accountFrom.hashCode() : 0;
+        result = 31 * result + (accountTo != null ? accountTo.hashCode() : 0);
+        result = 31 * result + sum;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Transfer{" +
+                "accountFrom=" + accountFrom +
+                ", accountTo=" + accountTo +
+                ", sum=" + sum +
+                '}';
     }
 }
